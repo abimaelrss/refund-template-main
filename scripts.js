dbe34975e4230b88a21e0563b88207a878a52ca9
Abimael <abimaelrss@gmail.com>
2024-12-07 20:27:12 -0300

    Accepting only numbers in value

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 // Seleciona os elementos do formulário
 const amount = document.getElementById("amount");
 
+// Captura o evento de input para formatar o valor
 amount.oninput = () => {
-  
+  // Obtem o valor atual do input e remove os caracteres não numéricos
+  let value = amount.value.replace(/\D/g, "");
+
+  // Atualiza o valor do input
+  amount.value = value;
 };
 
 
