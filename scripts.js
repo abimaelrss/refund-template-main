c530a42729bedab647d0a3ff7bc820df40f879bb
Abimael <abimaelrss@gmail.com>
2024-12-08 22:05:37 -0300

    Removing an item from the list

--- scripts.js
+++ scripts.js
@@ -161,6 +161,13 @@ function updateTotais(newExpense) {
 expenseList.addEventListener("click", function (event) {
   // Verifica se o elemento clicado é o ícone de remover
   if (event.target.classList.contains("remove-icon")) {
-    console.log(event)
+    // Obtem a li pai do elemento clicado
+    const item = event.target.closest(".expense");
+
+    // Remove o item da lista
+    item.remove();
   }
+
+  // Atualiza os tatais
+  updateTotais();
 });
